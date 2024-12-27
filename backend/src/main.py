bcbaa3115981c5ab0352bf618f341364241b87c5
El <sammaleen@gmail.com>
2024-12-27 11:30:46 +0300

    -

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,96 +1,173 @@
-from config.load_env import bot_token
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, BotCommand
+from telegram.ext import Application, CommandHandler, CallbackContext
 
+from flask import Flask, request, jsonify
+import threading
 import logging
 
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, Bot
-from telegram.constants import ParseMode
+from services.oauth_service import gen_oauth_link, store_state, get_user_id, get_token
+from services.asana_data import get_user_name, get_user_token
+from config.load_env import bot_token
 
-from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes
-from telegram.ext import filters
 
-# Logging configuration
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+# set logger and flask
+logging.basicConfig(
+    format = "%(asctime)s - %(name)s - %(levelname)s  - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
-# Pre-assign menu text
-FIRST_MENU = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button."
-SECOND_MENU = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons."
-
-# Pre-assign button text
-NEXT_BUTTON = "Next"
-BACK_BUTTON = "Back"
-TUTORIAL_BUTTON = "Tutorial"
-
-# Build keyboards
-FIRST_MENU_MARKUP = InlineKeyboardMarkup([[InlineKeyboardButton(NEXT_BUTTON, callback_data=NEXT_BUTTON)]])
-SECOND_MENU_MARKUP = InlineKeyboardMarkup(
-    [
-        [InlineKeyboardButton(BACK_BUTTON, callback_data=BACK_BUTTON)],
-        [InlineKeyboardButton(TUTORIAL_BUTTON, url="https://core.telegram.org/bots/api")],
-    ]
-)
+app = Flask(__name__)
 
 
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Handles the /start command."""
-    await update.message.reply_text("Hello! This is your bot.")
+# /START command handler
+async def start_command(update: Update, context: CallbackContext):
+    
+    oauth_link, state = gen_oauth_link()  # generate oauth link
+    store_state(update.effective_user.id, state) # store the state in Redis along with user_id mapping
+    logger.info(f"user - {update.effective_user.id}, state - {state}")
+    
+    keyboard = [[InlineKeyboardButton("Connect to Asana", url=oauth_link)]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    start_message = (
+        "Привет\!\n\n"
+        "Бот организует связь с Асаной для получения данных по запланированным задачам\.\n\n"
+        "Для начала работы авторизуйте свой аккаунт в Асане через кнопку *Connect to Asana* ниже или с помощью команды [/connect] \\. "
+        "Все доступные команды бота находятся в *Menu* внизу и здесь, в стартовом сообщении\.\n\n"
+        "*Команды*\n"
+        "[/start] \\- вернуться к стартовому сообщению\n"
+        "[/connect] \\- авторизоваться в Асане\n"
+
+    )
+        
+    await context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=open("C:/Users/samma/cursor/asana_bot/backend/src/assets/start.png", "rb"),
+        caption=start_message,
+        parse_mode="MarkdownV2",
+        reply_markup=reply_markup
+    )
+    
+    #await update.message.reply_text(start_message, 
+                                    #reply_markup=reply_markup,
+                                    #parse_mode="Markdown",
+                                    #chat_id=update.effective_chat.id,
+                                    #photo=open("C:/Users/samma/cursor/asana_bot/backend/src/assets/start.png", "rb"))
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo back user messages, optionally in uppercase."""
-    screaming = context.chat_data.get("screaming", False)
+# /CONNECT command handler
+async def connect_command(update: Update, context: CallbackContext):
+    
+    oauth_link, state = gen_oauth_link()  # generate oauth link
+    store_state(update.effective_user.id, state) # store the state in Redis along with user_id mapping
+    logger.info(f"user - {update.effective_user.id}, state - {state}")
+    
+    keyboard = [[InlineKeyboardButton("OAuth Link", url=oauth_link)]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    await update.message.reply_text("Перейдите по ссылке для авторизации:\n", reply_markup=reply_markup)
+   
+   
+# /MYTASKS command handler
+async def mytasks_command(update: Update, context: CallbackContext):
+    
+    keyboard = [[InlineKeyboardButton("Add note", url="")]]   
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    mytasks_message = ("")
+    
+    await update.message.reply_text(mytasks_message, reply_markup=reply_markup)
+   
+   
+# bot post initialization
+async def post_init(application: Application) -> None:
+    await application.bot.set_my_commands(
+        [BotCommand('start', 'go to start message'),
+         BotCommand('connect', 'connect to Asana'),
+         BotCommand('mytasks', 'get list of tasks for today')]
+        )
+    
+    menu_button = {
+    "type": "default",
+    "text": "Menu",  
+    }
+    
+    await application.bot.set_chat_menu_button()
 
-    if screaming and update.message.text:
-        await update.message.reply_text(update.message.text.upper())
+
+# OAuth callback route
+
+@app.route('/callback', methods=['GET'])
+
+async def callback():
+    
+    auth_code = request.args.get('code')
+    res_state = request.args.get('state')
+    
+    logger.info(f"recieved callback, state - {res_state}")
+    user_id = get_user_id(res_state) # fetch the user_id associated with the state
+    
+    if not user_id:
+        logger.error(f"invalid/expired state - {res_state}")
+        return "Invalid / Expired state", 400
+    
+    logger.info(f"valid state for user - {user_id}")
+    
+    access_token = get_token(auth_code) # Exchange auth code for access token
+    user_name = get_user_name(access_token)
+    
+    if access_token:
+        user_token = get_user_token(user_name)
+        
+        if user_token:
+            auth_message = (f"`auth successful`\n`user_name: {user_name}`\n"
+                            "`user_token extracted`") 
+        else:
+            auth_message = (f"`auth failed`\n`user_name: {user_name} - no asana token\n`"
+                            "[click here to set personal token](https://docs.google.com/spreadsheets/d/1w9pbRfUU2pPqiB8oAIUs5wqPxHMjxzMcJQ6aTL0WMtM/edit?usp=sharing)")
     else:
-        await update.message.reply_text(update.message.text)
+        auth_message = "`auth failed`\n`try to re-connect`"
+    
+    try:
+        application_instance = app.config['application_instance']
+        chat = await application_instance.bot.get_chat(user_id)
+        await chat.send_message(auth_message, parse_mode="Markdown")
+        
+    except Exception as e:
+        logger.error(f"error sending message to user {user_id}: {e}")
+
+    if access_token:
+        logger.info(f"got access_token for user - {user_id}")
+        return jsonify({"message": "auth successful", "access_token": access_token})
+    else:
+        logger.error(f"error exchanging code for user - {user_id}")
+        return "error exchanging code", 400
+    
+
+# run flask app in a separate thread to handle OAuth callback
+def start_flask_app(application):
+    app.config['application_instance'] = application  # pass application instance to Flask
+    app.run(port=5000, debug=True, use_reloader=False)
 
 
-async def scream(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Activate screaming mode."""
-    context.chat_data["screaming"] = True
-    await update.message.reply_text("Screaming mode activated!")
+# bot initialization 
+def main():
+    
+    application = Application.builder().token(bot_token).post_init(post_init).build()
+    
+    application.add_handler(CommandHandler("start", start_command)) # add /start
+    application.add_handler(CommandHandler("connect", connect_command)) # add /connect
+    application.add_handler(CommandHandler("mytasks", connect_command)) # add /mytasks
+    
+    # start flask app in a separate thread
+    flask_thread = threading.Thread(target=start_flask_app, args=(application,))
+    flask_thread.start()
 
-
-async def whisper(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Deactivate screaming mode."""
-    context.chat_data["screaming"] = False
-    await update.message.reply_text("Screaming mode deactivated!")
-
-
-async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send the first menu with inline buttons."""
-    await update.message.reply_text(FIRST_MENU, parse_mode=ParseMode.HTML, reply_markup=FIRST_MENU_MARKUP)
-
-
-async def button_tap(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Handle inline button presses."""
-    query = update.callback_query
-    await query.answer()
-
-    if query.data == NEXT_BUTTON:
-        await query.edit_message_text(SECOND_MENU, parse_mode=ParseMode.HTML, reply_markup=SECOND_MENU_MARKUP)
-    elif query.data == BACK_BUTTON:
-        await query.edit_message_text(FIRST_MENU, parse_mode=ParseMode.HTML, reply_markup=FIRST_MENU_MARKUP)
-
-
-def main() -> None:
-    """Main function to run the bot."""
-    # Initialize Application with token and default parse_mode
-    application = Application.builder().token(bot_token).build()
-
-    # Register handlers
-    application.add_handler(CommandHandler("start", start_command))
-    application.add_handler(CommandHandler("scream", scream))
-    application.add_handler(CommandHandler("whisper", whisper))
-    application.add_handler(CommandHandler("menu", menu))
-
-    application.add_handler(CallbackQueryHandler(button_tap))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot
+    # start bot
     application.run_polling()
 
 
 if __name__ == "__main__":
-    main()
+    main() 
+    
+    
--- backend/src/services/asana_data.py
+++ backend/src/services/asana_data.py
@@ -2,8 +2,12 @@ import requests
 import json
 import mysql.connector
 import pandas as pd
+import datetime
 
-# getting user name with exchanged token during authorization 
+from config.load_env import db_user, db_host, db_pass, database
+
+
+# GET USER NAME getting user name with exchanged token during authorization 
 def get_user_name(access_token):
     
     url = 'https://app.asana.com/api/1.0/users/me'
@@ -26,4 +30,113 @@ def get_user_name(access_token):
     return user_name
 
 
-# extracting asana data 
+# GET TASKS / extracting today tasks for a user
+def get_tasks(access_token, user_gid, workspace_gid):
+    
+    # get user's mytasks list gid
+    url = f"https://app.asana.com/api/1.0/users/{user_gid}/user_task_list"
+    headers = {'Authorization': f'Bearer {access_token}'}
+    
+    payload = {
+        'workspace': workspace_gid,
+        'opt_fields': '',
+        'opt_pretty': True  
+        }
+    
+    response = requests.get(url, headers=headers, params=payload)
+    status = response.status_code
+
+    if status == 200:
+        response_json = response.json()
+    else:
+        print(f'error: {status}')
+    
+    list_gid = response_json['data']['gid']
+    
+    # get my tasks for today
+    my_tasks = []
+    
+    url = f"https://app.asana.com/api/1.0/user_task_lists/{list_gid}/tasks"
+    
+    payload = {
+        'completed_since': 'now',
+        'opt_fields': 'name, created_at, due_on, start_on, projects, projects.name, section.name, notes, assignee_section.name, created_by.name, created_by.gid, permalink_url',
+        'limit': 100,
+        'opt_pretty': True  
+        }
+    
+     # pagination
+    while True:
+        response = requests.get(url, headers=headers, params=payload)
+        
+        if response.status_code == 200:
+            json_data = response.json()
+            
+            if json_data.get('data'): 
+                my_tasks.extend(json_data['data'])
+            
+            # check for more pages presence
+            if json_data.get('next_page'): 
+                payload['offset'] = json_data['next_page']['offset']  # update for next page
+            else:
+                break 
+        else:
+            print(f"error: {response.status_code}")
+            break
+        
+    if my_tasks:
+        my_tasks_df = pd.json_normalize(my_tasks, max_level=3) 
+        my_tasks_df.rename(columns={'gid':'task_gid',
+                                    'name':'task_name',
+                                    'permalink_url':'url',
+                                    'projects':'project_name'}, inplace=True)
+    
+        # SECTION NAME = TODAY и СЕГОДНЯ
+        my_tasks_df = my_tasks_df[(my_tasks_df['assignee_section.name'] == 'Today') 
+                                  | (my_tasks_df['assignee_section.name'] == 'Сегодня')]
+        
+        # extracting project names from nested list []
+        if 'project_name' in my_tasks_df.columns:
+            my_tasks_df['project_name'] = my_tasks_df['project_name'].apply(
+                lambda x: x[0]['name'] if isinstance(x, list) and x else '')
+        
+        #re-order columns
+        order = ['task_gid','project_name','task_name',
+                    'start_on','due_on','notes',
+                    'created_at','url','created_by.gid',
+                    'created_by.name','assignee_section.gid','assignee_section.name']
+        
+        my_tasks_df = my_tasks_df[order]
+        
+    else:
+        my_tasks_df = pd.DataFrame() 
+        
+    return my_tasks_df
+
+        
+# GET PERSONAL TOKEN from db - users
+def get_user_token(user_name):
+    
+    conn = mysql.connector.connect(user = db_user,
+                               password = db_pass,
+                               host = db_host,
+                               database = database)
+    
+    cursor = conn.cursor(dictionary=True)
+    
+    cursor.execute("SELECT user_token FROM users WHERE name = %s", 
+                   (user_name,))
+    
+    result = cursor.fetchone()
+    
+    cursor.close()
+    conn.close()
+    
+    if result:
+        return result['user_token']
+    else:
+        return None
+    
+# SAVE ASANA data to db - bot
+#def save_asana_data(user_name, user_token, tg_user_id):
+    
