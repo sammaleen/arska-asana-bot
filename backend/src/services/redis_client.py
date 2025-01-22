f435f579309085a218f946897b8686293b96acca
El <sammaleen@gmail.com>
2025-01-22 10:02:15 +0300

    -

--- backend/src/services/redis_client.py
+++ backend/src/services/redis_client.py
@@ -1,7 +1,15 @@
 import redis
-from config.load_env import rd_host, rd_port
+from config.load_env import rd_host, rd_port, db_user, db_pass
 
-redis_client = redis.Redis(host=rd_host, port=rd_port, decode_responses=True)
+redis_client = redis.Redis(host=rd_host,
+                           port=rd_port,
+                           decode_responses=True,
+                           username=db_user,
+                           password=db_pass)
 
 def get_redis_client():
     return redis_client
+
+success = redis_client.set('foo', 'bar')
+result = redis_client.get('foo')
+print(result)
