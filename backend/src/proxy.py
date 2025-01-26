597de280af6c84f5cc8ba2e50639e12ac745e4df
El <sammaleen@gmail.com>
2025-01-26 22:54:46 +0300

    -

--- backend/src/proxy.py
+++ backend/src/proxy.py
@@ -6,11 +6,11 @@ proxy_app = Flask(__name__)
 @proxy_app.route('/callback', methods=['GET'])
 def callback():
     url = "https://arska-sammaleen.eu.pythonanywhere.com/callback"
-    params = request.args  # Forward query parameters
-    headers = dict(request.headers)  # Convert headers to a mutable dictionary
+    params = request.args  
+    headers = dict(request.headers)  # Convert headers to a mutable dict
     try:
         response = requests.get(url, params=params, headers=headers, timeout=10)
         content_type = response.headers.get('Content-Type', 'text/plain')
         return Response(response.content, status=response.status_code, content_type=content_type)
-    except requests.RequestException as e:
-        return Response(f"Error: {str(e)}", status=500, content_type='text/plain')
+    except requests.RequestException as err:
+        return Response(f"Error: {str(err)}", status=500, content_type='text/plain')
