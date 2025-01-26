ae787b6b8a0bce7d4c1b779ded1e46b901cfebe3
El <sammaleen@gmail.com>
2025-01-26 22:40:06 +0300

    -

--- backend/src/proxy.py
+++ backend/src/proxy.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, jsonify
 import requests
+from flask import Flask, request, Response
 
 proxy_app = Flask(__name__)
 
-# proxy the oauth callaback request to the main flask app in main.py
 @proxy_app.route('/callback', methods=['GET'])
 def callback():
-    response = request.get("http://127.0.0.1:8000/callback", params=request.args)
-    
-    return (response.text, response.status_code, response.headers.items())
-    
+    url = "http://127.0.0.1:8000/callback"
+    params = request.args  
+    headers = dict(request.headers)  # convert headers to a mutable dict
+    response = requests.get(url, params=params, headers=headers) 
+    content_type = response.headers.get('Content-Type', 'text/plain')
+    return Response(response.content, status=response.status_code, content_type=content_type)
