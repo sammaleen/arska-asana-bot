8d28cdff1a0ad3e7c04032d4e6ae835a54022064
El <sammaleen@gmail.com>
2025-01-29 17:50:52 +0300

    -

--- backend/src/services/asana_data.py
+++ backend/src/services/asana_data.py
@@ -500,7 +500,8 @@ def format_report(user_df, user, tg_user_name, max_len=None, max_note_len=None):
     current_date = datetime.now().strftime("%d %b %Y · %a")
     
     if tg_user_name:
-        message = f"*{user}* `@{tg_user_name}`\n{current_date}\n\n"
+        tg_user_name_esc = tg_user_name.replace("_", "\\_")  # escape '_'
+        message = f"*{user}* `@{tg_user_name_esc}`\n{current_date}\n\n"
     else:
         message = f"*{user}*\n{current_date}\n\n"
     
